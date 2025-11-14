62adcca627d03d4830b22cccf5cdc96bfcece19d
Eirik Lillebo <erlillebo@gmail.com>
2025-11-14 16:08:43 +0100

    feat: TagList

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export { default as CopyButton } from './components/copy-button';
 export { default as HStack } from './components/hstack';
 export { default as VStack } from './components/vstack';
 export { HelpText } from './components/helptext';
+export { default as TagList } from './components/tag-list';
