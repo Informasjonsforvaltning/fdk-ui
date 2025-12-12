642e24253bce53ef76afab19c416316fe4baa5d3
Eirik Lillebo <erlillebo@gmail.com>
2025-12-12 10:07:28 +0100

    fix: added storybook story to demonstrate TagList maxTags prop

--- src/components/tag-list/tag-list.stories.tsx
+++ src/components/tag-list/tag-list.stories.tsx
@@ -21,30 +21,84 @@ export const Primary: Story = {
         <>
             <div style={{ padding: '1rem' }}>
                 <TagList>
-                    <Tag size='sm'>My tag</Tag>
+                    <Tag data-size='sm'>My tag</Tag>
                     <Tag
-                        size='sm'
+                        data-size='sm'
                         color='success'
                     >
                         My tag
                     </Tag>
                     <Tag
-                        size='sm'
+                        data-size='sm'
                         color='neutral'
                     >
                         My tag
                     </Tag>
                     <Tag
-                        size='sm'
+                        data-size='sm'
                         color='warning'
                     >
                         My tag
                     </Tag>
                     <Tag
-                        size='sm'
+                        data-size='sm'
                         color='danger'
                     >
                         My tag
+                    </Tag>
+                </TagList>
+            </div>
+        </>
+    ),
+};
+
+export const WithMaxTags: Story = {
+    parameters: {
+        nextjs: {
+            appDirectory: true,
+        },
+    },
+    render: () => (
+        <>
+            <div style={{ padding: '1rem' }}>
+                <TagList maxTags={3}>
+                    <Tag data-size='sm'>Tag 1</Tag>
+                    <Tag
+                        data-size='sm'
+                        color='success'
+                    >
+                        Tag 2
+                    </Tag>
+                    <Tag
+                        data-size='sm'
+                        color='neutral'
+                    >
+                        Tag 3
+                    </Tag>
+                    <Tag
+                        data-size='sm'
+                        color='warning'
+                    >
+                        Tag 4
+                    </Tag>
+                    <Tag
+                        data-size='sm'
+                        color='danger'
+                    >
+                        Tag 5
+                    </Tag>
+                    <Tag data-size='sm'>Tag 6</Tag>
+                    <Tag
+                        data-size='sm'
+                        color='success'
+                    >
+                        Tag 7
+                    </Tag>
+                    <Tag
+                        data-size='sm'
+                        color='neutral'
+                    >
+                        Tag 8
                     </Tag>
                 </TagList>
             </div>
