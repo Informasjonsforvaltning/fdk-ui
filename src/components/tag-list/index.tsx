b880c23e879448590cab4fd445e0d21ec73e7b93
Eirik Lillebo <erlillebo@gmail.com>
2025-12-12 10:07:49 +0100

    fix: TagList use child key if present

--- src/components/tag-list/index.tsx
+++ src/components/tag-list/index.tsx
@@ -18,12 +18,15 @@ const TagList = ({ children, className, maxTags, ...props }: TagListProps & Reac
             {childArray
                 .filter((child) => Boolean(child))
                 .slice(0, maxTags ?? childArray.length)
-                .map((child, i) => (
-                    <li key={i}>{child}</li>
-                ))}
+                .map((child, i) => {
+                    const key = React.isValidElement(child) && child.key != null 
+                        ? child.key 
+                        : i;
+                    return <li key={key}>{child}</li>;
+                })}
             {maxTags && maxTags < childArray.length && (
                 <li>
-                    <Tag>
+                    <Tag data-size='sm'>
                         <span>+{childArray.length - maxTags}</span>
                     </Tag>
                 </li>
