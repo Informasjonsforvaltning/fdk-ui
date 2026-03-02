0a87155d0d6ce91d19692c3e37aa0a24b12ac052
Øyvind Julsrud <oyvjul@gmail.com>
2026-03-02 13:20:50 +0100

    chore: upgrade storybook 8.6 to 9

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,10 +6,10 @@ const config: StorybookConfig = {
     "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
   "addons": [
-    "@storybook/addon-essentials",
+    "@storybook/addon-docs",
     "@storybook/addon-onboarding",
     "@chromatic-com/storybook",
-    "@storybook/experimental-addon-test"
+    "@storybook/addon-vitest"
   ],
   "framework": {
     "name": "@storybook/react-vite",
